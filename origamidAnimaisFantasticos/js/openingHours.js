5a7e41b1e2d8795d0288083c9dc512b53764e5b3
alexmuniz96 <alexmuniz096@gmail.com>
2021-08-25 19:42:18 -0300

    adding the visual reference to hours of operation

--- origamidAnimaisFantasticos/js/openingHours.js
+++ origamidAnimaisFantasticos/js/openingHours.js
@@ -1,20 +1,17 @@
 export default function initOpeningHours(){
+  const operation = document.querySelector("[data-week='1,2,3,4,5']")
+  const dayWeek = operation.dataset.week.split(",").map(Number)
+  const hours = operation.dataset.time.split(",").map(Number)
+  const presentDate = new Date()
+  const presentDayWeek = presentDate.getDay()
+  const presentHours = presentDate.getHours()
+  const weekIsOpen = dayWeek.indexOf(presentDayWeek) !== -1
+  const hoursIsOpen = presentHours > hours[0] && presentHours < hours[1]
   
+  if(weekIsOpen && hoursIsOpen){
+    operation.classList.add("open")
+  } else{
+    operation.classList.add("close")
+  }
 }
 
-const operation = document.querySelector("[data-week='1,2,3,4,5']")
-const dayWeek = operation.dataset.week.split(",").map(Number)
-const hours = operation.dataset.time.split(",").map(Number)
-const presentDate = new Date()
-const presentDayWeek = presentDate.getDay()
-const presentHours = presentDate.getHours()
-
-console.log(presentDayWeek)
-console.log(dayWeek)
-
-const weekIsOpen = dayWeek.indexOf(presentDayWeek) !== -1
-const hoursIsOpen = presentHours > hours[0] && presentHours < hours[1]
-
-if(weekIsOpen && hoursIsOpen){
-  console.log("estamos funcionando")
-}
