583fa3a46af29b2e3eacf065889970213e4dc420
alexmuniz96 <alexmuniz096@gmail.com>
2021-09-16 21:01:36 -0300

    study loops-iterable

---
+++ loops-iterable/script.js
@@ -0,0 +1,15 @@
+// Crie 4 li's na página
+// Utilizando o for...of
+// adicione uma classe a cada li
+
+const lis = document.querySelectorAll("li")
+
+for(const li of lis){
+  li.classList.add("active")
+}
+// Utilize o for...in para listar
+// todos as propriedades e valores
+// do objeto window
+for(const windowKey in window){
+  console.log(windowKey + " : "+ window[windowKey])
+}
