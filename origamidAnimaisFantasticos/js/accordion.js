a6e79df7f3d2f3e6e2c459771bd5a4983798115b
alexmuniz96 <alexmuniz096@gmail.com>
2021-10-05 17:45:11 -0300

    refactoring accordion class

--- origamidAnimaisFantasticos/js/accordion.js
+++ origamidAnimaisFantasticos/js/accordion.js
@@ -1,20 +1,24 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    "[data-anime='accordion'] dt"
-  );
-  const activeClass = "ativo";
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo"
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    accordionList.forEach((el) => {
-      el.addEventListener("click", activeAccordion);
+  toggleAccordion(el) {
+    el.classList.toggle(this.activeClass);
+    el.nextElementSibling.classList.toggle(this.activeClass);
+  }
+  //adiciona o evento no accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((el) => {
+      el.addEventListener("click", () => this.toggleAccordion(el));
     });
+  }
+  //Adicionar função
+  init() {
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0])
+      this.addAccordionEvent()
+    }
   }
 }
