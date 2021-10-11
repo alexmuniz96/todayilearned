793f50db5f3c2eabac22c39120b6c13305f5a91b
alexmuniz96 <alexmuniz096@gmail.com>
2021-10-11 15:39:22 -0300

    refactoring numbersAnimate into class

--- origamidAnimaisFantasticos/js/fetchAnimais.js
+++ origamidAnimaisFantasticos/js/fetchAnimais.js
@@ -1,24 +1,25 @@
-import initNumbersAnimate from "./numbersAnimate.js"
+import NumbersAnimate from "./numbersAnimate.js"
 
-export default function initFetchAnimais(){
+export default function initFetchAnimais() {
   const numerosGrid = document.querySelector(".numeros-grid")
-  async function getAnimais(url){
-    try{
+  async function getAnimais(url) {
+    try {
       const responseAnimais = await fetch(url)
       const jsonAnimais = await responseAnimais.json()
       jsonAnimais.forEach(animal => {
         const divAnimal = createAnimais(animal)
         numerosGrid.appendChild(divAnimal)
       });
-    initNumbersAnimate()
-  }catch(error){
-    console.log(error)
+      const numberAnimate = new NumbersAnimate("[data-numero]", ".numeros", "ativo")
+      numberAnimate.init()
+    } catch (error) {
+      console.log(error)
+    }
   }
-  }
-  
+
   getAnimais("../animais.json")
 
-  function createAnimais(animal){
+  function createAnimais(animal) {
     const div = document.createElement("div")
     div.classList.add("numero-animal")
     div.innerHTML = `<h3>${animal.specie}</h3> <span data-numero> ${animal.number}</span>`
